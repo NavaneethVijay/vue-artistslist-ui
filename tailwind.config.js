9309ec51b10bd2753b829b9e496b70d098c0a68c
Navaneeth V <navaneeth.v@codilar.com>
2020-08-12 22:53:13 +0530

    purge update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,4 @@ module.exports = {
   theme: {},
   variants: {},
   plugins: [],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
 }
